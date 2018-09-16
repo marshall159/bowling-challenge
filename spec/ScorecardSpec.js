9015178a677bcda2a31800cd32ebda6f943f88e7
Aneel Marshall <aneel.marshall@outlook.com>
2018-09-16 13:26:33 +0100

    add addAllFrames

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -1,23 +1,25 @@
 'use strict';
 
 describe('Scorecard', function() {
-    var card, firstFrame;
+    var card, firstFrame, spyFrame;
 
     beforeEach(function() {
         card = new Scorecard();
-        firstFrame = new Frame();
+        spyFrame = jasmine.createSpyObj('frame', ['pinsKnockedDown']);
     });
 
 
     it('adds a frame', function() {
+        firstFrame = new Frame();
         card.addFrame(firstFrame);
         expect(card.frames).toContain(firstFrame);
     });
 
-    // it('has ten frames', function() {
-    //   expect(card.frames.length).toEqual(10);
-    // });
-    //
+    it('has ten frames', function() {
+        card.addAllFrames();
+        expect(card.frames.length).toEqual(10);
+    });
+
     // it('writes input to frames property', function() {
     //   expect(card.frames[0]).toEqual(1);
     // });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -4,8 +4,8 @@ function Scorecard() {
     this.frames = [];
 }
 
-Scorecard.prototype.addFrame = function () {
-    this.frames.push(new Frame());
+Scorecard.prototype.addFrame = function(frame) {
+    this.frames.push(frame);
 };
 
 // Scorecard.prototype.writeFrames = function () {
@@ -13,3 +13,12 @@ Scorecard.prototype.addFrame = function () {
 //     this.frames[i] = this.pinsKnockedDown();
 //   }
 // };
+
+Scorecard.prototype.addAllFrames = function(numFrames = 10) {
+    for (let i = 0; i < numFrames; i++) {
+        let frame = new Frame();
+        frame.pinsKnockedDown(5);
+        frame.pinsKnockedDown(5);
+        this.addFrame(frame);
+    }
+}
